e25d550d50b75d1c23fe9edcd1686c529a551d97
ndavis <nadiadavis@protonmail.com>
2020-09-29 17:07:13 -0500

    Change: Title screen music

--- src/title_screen.c
+++ src/title_screen.c
@@ -609,7 +609,7 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        m4aSongNumStart(MUS_TITLE);
+        m4aSongNumStart(MUS_RG_TITLE);
         gMain.state = 5;
         break;
     case 5:
