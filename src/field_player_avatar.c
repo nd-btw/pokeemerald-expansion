728a2d6e720ff2956b99af69d98411b085fde704
ndavis <nadiadavis@protonmail.com>
2020-09-29 16:31:18 -0500

    Fix: Surf Dismount Ground Effects

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1666,7 +1666,8 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         ScriptContext2_Disable();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
-        DestroyTask(taskId);
+	playerObjEvent->triggerGroundEffectsOnMove = TRUE;
+	DestroyTask(taskId);
     }
 }
 
